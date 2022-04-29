2c18a3030936bea7717c0fad703145df9f935ecf
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-04-29 15:25:22 -0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useState } from "react";
 import { useState } from "react";
 import { useState } from "react";
+import { useState } from "react";
 
 export function Button(){
     const [counter, setCounter] = useState(0) //variavel 
